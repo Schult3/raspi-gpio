b510fd489e55ed9cd46d406db269ac53fab322b2
Stefan Schulte <damm.schulte@gmail.com>
2017-12-30 11:36:54 +0100

    b

--- mic/ws2812b.py
+++ mic/ws2812b.py
@@ -151,12 +151,14 @@ def runningLights(strip, color, wait_ms=50, anz_cars = 1, car_length = 5, car_sp
 def initializeTetris(strip, color, wait_ms=50, parts=1):
     queue = 0
     i = strip.numPixels() - 1
-    while i >= queue:
-        strip.setPixelColor(i, color)
-        strip.setPixelColor(i + 1, Color(0, 0, 0))
-        strip.show()
-        i -= 1
-        time.sleep(wait_ms / 1000.0)
+    while queue <= strip.numPixels() - 1:
+        while i >= queue:
+            strip.setPixelColor(i, color)
+            strip.setPixelColor(i + 1, Color(0, 0, 0))
+            strip.show()
+            i -= 1
+            time.sleep(wait_ms / 1000.0)
+        queue += 1
 
 
 
