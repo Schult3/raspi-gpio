b75401555c00859c4362c76c2f306464ddcb83a6
Stefan Schulte <damm.schulte@gmail.com>
2017-12-30 11:27:18 +0100

    b

--- mic/ws2812b.py
+++ mic/ws2812b.py
@@ -166,7 +166,7 @@ if __name__ == '__main__':
 	#runningLights(strip, Color(0, 160, 245), 10, 3, 3, 25)
 	#destroy(strip, Color(0, 0, 0))
 
-	while True:
+    while True:
 		#rainbow(strip)
 		#strobe(strip, Color(255, 255, 255), 20)
 		#equalizer(strip)
