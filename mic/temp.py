befb7a969bdb4a4437738d749034e6e0084bfe25
Stefan Schulte <damm.schulte@gmail.com>
2018-01-02 15:42:48 +0100

    temp control

---
+++ mic/temp.py
@@ -0,0 +1,31 @@
+
+import re, os, rrdtool, time
+
+# function: read and parse sensor data file
+def read_sensor(path):
+  value = "U"
+  try:
+    f = open(path, "r")
+    line = f.readline()
+    if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+      line = f.readline()
+      m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+      if m:
+        value = str(float(m.group(2)) / 1000.0)
+    f.close()
+  except (IOError), e:
+    print time.strftime("%x %X"), "Error reading", path, ": ", e
+  return value
+
+# define pathes to 1-wire sensor data
+pathes = (
+  "/sys/bus/w1/devices/28-000005a2120b/w1_slave"e"
+)
+
+# read sensor data
+data = 'N'
+for path in pathes:
+  data += ':'
+  data += read_sensor(path)
+  print(data)
+  time.sleep(1)
