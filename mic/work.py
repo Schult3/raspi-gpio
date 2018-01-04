63bda0e442fce7734a3f5b74be8a1ba2a03fefb5
Stefan Schulte <damm.schulte@gmail.com>
2018-01-04 13:48:29 +0100

    bug

--- mic/work.py
+++ mic/work.py
@@ -1,53 +1,197 @@
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
 import time
-import RPi.GPIO as GPIO
 import sound_analysis as sa
-import killswitch as k
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-LED1 = 11
-LED2 = 13
-LED3 = 15
-GPIO.setup(LED1, GPIO.OUT)
-GPIO.setup(LED2, GPIO.OUT)
-GPIO.setup(LED3, GPIO.OUT)
+from neopixel import *
 
-pwm1 = GPIO.PWM(LED1, 1000)
-pwm1.start(0)
+import argparse
+import signal
+import sys
+def signal_handler(signal, frame):
+        colorWipe(strip, Color(0,0,0))
+        sys.exit(0)
 
-pwm2 = GPIO.PWM(LED2, 1000)
-pwm2.start(0)
+def opt_parse():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-c', action='store_true', help='clear the display on exit')
+        args = parser.parse_args()
+        if args.c:
+                signal.signal(signal.SIGINT, signal_handler)
 
-pwm3 = GPIO.PWM(LED3, 1000)
-pwm3.start(0)
+# LED strip configuration:
+LED_COUNT      = 134      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
-def main():
-	power_amp = 0
+
+# Equalizer Einstellungen
+HIST_AMP = 0
+POS_OFFSET = []
+
+# Tetris Einstellungen
+TET_QUEUE = 0
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def initialize(strip, color, wait_ms=10):
+	x = strip.numPixels() / 2
+	for i in range(x):
+		strip.setPixelColor(i, color)
+		strip.setPixelColor(strip.numPixels() - i - 1, color)
+		strip.show()
+		time.sleep(wait_ms / 1000.0)
+	time.sleep(wait_ms / 1000.0)
+
+def destroy(strip, color, wait_ms=10):
+	x = strip.numPixels() / 2
+	while x >= 0:
+		strip.setPixelColor(x, color)
+		strip.setPixelColor(strip.numPixels() - x - 1, color)
+		strip.show()
+		time.sleep(wait_ms / 1000.0)
+		x -= 1
+
+
+def rainbow(strip, wait_ms=20, iterations=1):
+	"""Draw rainbow that fades across all pixels at once."""
+    	for j in range(256*iterations):
+        	for i in range(strip.numPixels()):
+            		strip.setPixelColor(i, wheel((i+j) & 255))
+        	strip.show()
+        	time.sleep(wait_ms/1000.0)
+
+def equalizer(strip, parts=2):
+	global HIST_AMP
+	global POS_OFFSET
+
+	amp = sa.getSoundPWM()
+	max_pixels = strip.numPixels() / parts
+	anz_pixels = amp / 100.0 * max_pixels
+	anz_pixels = round(anz_pixels, 0)
+
+	if len(POS_OFFSET) != parts:
+		for i in range(parts):
+			POS_OFFSET.append(i * max_pixels)
+
+	print(POS_OFFSET)
+
+	hist_anz_pixels = HIST_AMP / 100.0 * max_pixels
+	hist_anz_pixels = round(hist_anz_pixels, 0)
+	if amp > HIST_AMP:
+		i = int(hist_anz_pixels)
+		while i <= anz_pixels:
+			for x in POS_OFFSET:
+				strip.setPixelColor(x + i, wheel(x + i))
+			strip.show()
+			i += 1
+	else:
+		i = int(hist_anz_pixels)
+		while i >= anz_pixels:
+			for x in POS_OFFSET:
+				strip.setPixelColor(x + i, Color(0, 0, 0))
+			strip.show()
+			i -= 1
+
+	HIST_AMP = amp
+	#time.sleep(10 / 1000.0)
+
+def strobe(strip, color, wait_ms=50):
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+	strip.show()
+	time.sleep(wait_ms / 1000.0)
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, Color(0, 0, 0))
+	strip.show()
+	time.sleep(wait_ms / 1000.0)
+
+
+def runningLights(strip, color, wait_ms=50, anz_cars = 1, car_length = 5, car_space = 10):
+	cars = []
+	pos_offset = 0
+	for i in range(anz_cars):
+		pos = []
+		for x in range(car_length):
+			pos.append(pos_offset)
+			pos_offset -= 1
+		cars.append(pos)
+		pos_offset-= car_space
 
 	while True:
-		#k.killswitch()
-		power = sa.getSoundPWM()
-		print(power)
-		if power >= 75:
-			pwm3.ChangeDutyCycle(power)
-			pwm2.ChangeDutyCycle(100)
-			pwm1.ChangeDutyCycle(100)
-		elif power >= 50:
-			pwm2.ChangeDutyCycle(power)
-			pwm3.ChangeDutyCycle(0)
-			pwm1.ChangeDutyCycle(100)
-		elif power >= 20:
-			pwm1.ChangeDutyCycle(power)
-			pwm2.ChangeDutyCycle(0)
-			pwm3.ChangeDutyCycle(0)
-			power_amp = power
-		else:
-			power_amp -= 5
-			if power_amp <= 20:
-				power_amp = 20
-			pwm1.ChangeDutyCycle(power_amp)
-			pwm2.ChangeDutyCycle(0)
-			pwm3.ChangeDutyCycle(0)
+		for x in range(strip.numPixels()):
+			strip.setPixelColor(x, Color(0, 0, 0))
 
+		for i in cars:
+			for pos in i:
+				strip.setPixelColor(pos, color)
+				if pos + 1 >= strip.numPixels():
+					cars[cars.index(i)][i.index(pos)] = 0
+				else:
+					cars[cars.index(i)][i.index(pos)] += 1
+		strip.show()
+		time.sleep(wait_ms / 1000.0)
+
+def initializeTetris(strip, color, wait_ms=25, parts=1):
+    global TET_QUEUE
+    i = strip.numPixels() - 1
+    while i >= TET_QUEUE:
+        strip.setPixelColor(i, color)
+        strip.setPixelColor(i + 1, Color(0, 0, 0))
+        strip.show()
+        i -= 1
+        time.sleep(wait_ms / 1000.0)
+
+    if TET_QUEUE >= strip.numPixels() - 1:
+        TET_QUEUE = 0
+    else:
+        TET_QUEUE += 1
+
+
+
+# Main program logic follows:
 if __name__ == '__main__':
-	main()
+    # Process arguments
+    opt_parse()
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    initialize(strip, Color(255, 255, 255))
+	#runningLights(strip, Color(0, 160, 245), 10, 3, 3, 25)
+	#destroy(strip, Color(0, 0, 0))
+
+    rainbow_counter = 0
+
+    while True:
+		#rainbow(strip)
+		#strobe(strip, Color(255, 255, 255), 20)
+		#equalizer(strip)
+		#destroy(strip, Color(0, 0, 0))
+		#initialize(strip, Color(0, 160, 245))
+		#runningLights(strip, Color(0, 160, 245))
+        initializeTetris(strip, wheel(rainbow_counter), 10)
+        if rainbow_counter >= 255:
+            rainbow_counter = 0
+        else:
+            rainbow_counter += 1
