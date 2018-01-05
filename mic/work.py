3621973bd32724519bea25bf987863c5e823ee88
Stefan Schulte <damm.schulte@gmail.com>
2018-01-05 16:21:48 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -104,6 +104,8 @@ def rainbow(strip, color):
 
 
     strip.setPixelColor(RB_I, wheel((RB_I + RB_J) & 255))
+    print(RB_I + RB_J)
+    
     RB_I += 1
     if RB_I >= strip.numPixels():
         RB_I = 0
