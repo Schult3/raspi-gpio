bd3da9b87382a72cf15225c1363a9d83158beb4b
Stefan Schulte <damm.schulte@gmail.com>
2018-01-05 11:10:21 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -240,7 +240,7 @@ if __name__ == '__main__':
         	equalizer(strip, 4)
         else:
             if effect_counter <= 1:
-                randint = random.randint(0, len(effects) - 1)
+                randint = random.randint(0, len(effects)) - 1
             effects[randint](strip, color)
 
 
