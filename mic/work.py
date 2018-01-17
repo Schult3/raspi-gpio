26406d9b999f042897d9f86c92227a681033cd20
Stefan Schulte <damm.schulte@gmail.com>
2018-01-17 17:59:13 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -317,8 +317,8 @@ def runningCircle(strip, color):
             RC_LIST.append(i)
             strip.setPixelColor(i, Color(0, 0, 0))
         randint = random.randint(0, strip.numPixels())
-        RC_OFFSET_NEG = randint
-        RC_OFFSET_POS = randint
+        RC_OFFSET_NEG = randint - 1
+        RC_OFFSET_POS = randint + 1
 
         del RC_LIST[randint]
         for i in RC_LIST:
