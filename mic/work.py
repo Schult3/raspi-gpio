a5bdb07dd6c4efaa45ef62547abe27033580bea2
Stefan Schulte <damm.schulte@gmail.com>
2018-01-20 11:28:11 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -233,13 +233,14 @@ def strobe(strip, color):
         for i in range(strip.numPixels()):
         	strip.setPixelColor(i, Color(255, 255, 255))
         strip.show()
-        time.sleep(25 / 1000.0)
+        time.sleep(10 / 1000.0)
         for i in range(strip.numPixels()):
         	strip.setPixelColor(i, Color(0, 0, 0))
         strip.show()
 
     AKT_MODUS = "ST"
     FLG_CHANGE_EFFECT = 1
+    time.sleep(25 / 1000.0)
 
 
 
