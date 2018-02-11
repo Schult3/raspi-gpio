d142b9714d0334f98ac607d32843babd9dc4d1ad
Stefan Schulte <damm.schulte@gmail.com>
2018-02-11 16:26:41 +0100

    b

--- mic/killswitch.py
+++ mic/killswitch.py
@@ -8,14 +8,12 @@ KILLSWITCH = 18
 GPIO.setup(KILLSWITCH, GPIO.IN)
 
 def killswitch():
-	print(GPIO.input(KILLSWITCH))
-	#if GPIO.input(KILLSWITCH) != GPIO.HIGH:
-		#os.system("sudo shutdown -h now")
+	if GPIO.input(KILLSWITCH) != GPIO.HIGH:
+		os.system("sudo shutdown -h now")
 
 def main():
 	while True:
 		killswitch()
-		time.sleep(0.2)
 
 if __name__ == "__main__":
 	main()
