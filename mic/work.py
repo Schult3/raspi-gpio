5afb0f0a5adf0af71311b57849722416c417c4a7
Stefan Schulte <damm.schulte@gmail.com>
2018-02-14 19:28:45 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -638,7 +638,8 @@ if __name__ == '__main__':
     strip.begin()
     #initialize(strip, Color(255, 255, 255))
 
-    effects = [initializeTetris, runningLights, chrystal, rainbow, runningCircle, theatreChase]
+    #effects = [initializeTetris, runningLights, chrystal, rainbow, runningCircle, theatreChase]
+    effects = [initializeTetris]
     music_effects = [equalizer, strobe, SoundPulse]
 
     rainbow_counter = 0
