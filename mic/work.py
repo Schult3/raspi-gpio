72c17040dc54dcbcec90c433623b04b6e59d9c39
Stefan Schulte <damm.schulte@gmail.com>
2018-02-17 13:18:51 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -677,8 +677,6 @@ if __name__ == '__main__':
             if EFFECT_COUNTER <= 1 or EFFECT_COUNTER > 10:
                 randomizeEffectCounter(2, 25)
                 effectNum = random.randint(0, len(effects) - 1)
-                print(effectNum)
-                print("---")
             effects[effectNum](strip, color)
             time.sleep(config["range_delay"] / 1000.0)
 
