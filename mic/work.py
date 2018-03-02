b53cc36a1273478b0768ffc081cfe3676324bb78
Stefan Schulte <damm.schulte@gmail.com>
2018-03-02 16:35:31 +0100

    b

--- mic/work.py
+++ mic/work.py
@@ -682,12 +682,13 @@ if __name__ == '__main__':
             light(strip, color)
             time.sleep(config["range_delay"] / 1000.0)
         elif config["singleeffect_switch"] == True:
-            print(config["select_effect"])
             sEffect = config["select_effect"]
             if sEffect in effectsNames:
-                print("Test")
                 effects[effectsNames.index(sEffect)](strip, color)
                 time.sleep(config["range_delay"] / 1000.0)
+
+            if sEffect in music_effectsNames:
+                music_effects[music_effectsNames.index(sEffect)](strip, color)
 
         elif config["music_switch"] == True:
             if EFFECT_COUNTER <= 1:
